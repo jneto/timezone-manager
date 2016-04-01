190db679ea96a94de637e297ee72782c6a6f86c5
João Neto <joao.fernandesn@gmail.com>
2016-04-01 14:58:44 -0300

    Refactor UserEditPage container.

--- app/react/containers/UserEditPage.js
+++ app/react/containers/UserEditPage.js
@@ -1,46 +1,36 @@
-import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { saveUser, createUser } from '../actions/users'
 
 import UserEdit from '../components/UserEdit'
 
-class UserEditPage extends Component {
-    constructor(props) {
-        super(props)
-        this.onSubmit = this.onSubmit.bind(this)
-    }
-
-    componentWillMount() {
-        const { users, params } = this.props
-        this.user = users.find((user) => {return user._id === params.id})
-        if (!this.user) {
-            this.user = {
-                username: '',
-                role: 'REGULAR'
-            }
-        }
-        console.log(this.user)
-    }
-
-    onSubmit(role, password, confirmPassword, username) {
-        if (this.user._id) {
-            this.props.dispatch(saveUser(role, password, confirmPassword, this.user._id))
-        } else {
-            this.props.dispatch(createUser(role, password, confirmPassword, username))
+const mapStateToProps = (state, ownProps) => {
+    let user = state.users.list.find((user) => {return user._id === ownProps.params.id})
+    if (!user) {
+        user = {
+            username: '',
+            role: 'REGULAR'
         }
     }
-
-    render() {
-        return (<UserEdit user={this.user} onSubmit={this.onSubmit} message={this.props.message}/>)
-    }
-}
-
-const mapStateToProps = state => {
     return {
-        users: state.users.list,
+        user: user,
         message: state.users.message
     }
 }
 
-export default connect(mapStateToProps)(UserEditPage)
+const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        onSubmit: (role, password, confirmPassword, username) => {
+            console.log(ownProps)
+            if (ownProps.params && ownProps.params.id) {
+                dispatch(saveUser(role, password, confirmPassword, ownProps.params.id))
+            } else {
+                dispatch(createUser(role, password, confirmPassword, username))
+            }
+        }
+    }
+}
+
+const UserEditPage = connect(mapStateToProps, mapDispatchToProps)(UserEdit)
+
+export default UserEditPage
